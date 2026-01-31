85f6e33ca3c0744aeacd4498065a9c26dce52ad0
SamMcAulay <SamMcAulay@pm.me>
2026-01-31 23:21:29 +0000

    Initialised NestJS backend, With Swagger and core rescources

---
+++ backend/src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // SWAGGER CONFIGURATION
+  const config = new DocumentBuilder()
+    .setTitle('AquaFit Pro API')
+    .setDescription('The backend API for the booking system')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  
+  await app.listen(3000);
+}
+bootstrap();
